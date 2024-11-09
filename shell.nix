8a1f2046b3f6546fcf670b0ef517f829c3cdbc25
theverygaming <theverygaming@users.noreply.github.com>
2024-11-09 05:42:51 +0100

    stuff

--- shell.nix
+++ shell.nix
@@ -1,43 +1,6 @@
 # TODO: switch to flake
-# from https://nixos.wiki/wiki/Rust - MIT - Copyright (c) 2017 Jörg Thalheim
-{ pkgs ? import <nixpkgs> { } }:
 let
-  overrides = (builtins.fromTOML (builtins.readFile ./rust-toolchain.toml));
-  libPath = with pkgs; lib.makeLibraryPath [
-    # load external libraries that you need in your rust project here
-  ];
-in
-pkgs.mkShell rec {
-  buildInputs = with pkgs; [
-    clang
-    # Replace llvmPackages with llvmPackages_X, where X is the latest LLVM version (at the time of writing, 16)
-    llvmPackages.bintools
-    rustup
-    diesel-cli
-  ];
-  RUSTC_VERSION = overrides.toolchain.channel;
-  # https://github.com/rust-lang/rust-bindgen#environment-variables
-  LIBCLANG_PATH = pkgs.lib.makeLibraryPath [ pkgs.llvmPackages_latest.libclang.lib ];
-  shellHook = ''
-    export PATH=$PATH:''${CARGO_HOME:-~/.cargo}/bin
-    export PATH=$PATH:''${RUSTUP_HOME:-~/.rustup}/toolchains/$RUSTC_VERSION-x86_64-unknown-linux-gnu/bin/
-  '';
-  # Add precompiled library to rustc search path
-  RUSTFLAGS = (builtins.map (a: ''-L ${a}/lib'') [
-    pkgs.postgresql.lib
-  ]);
-  LD_LIBRARY_PATH = libPath;
-  # Add glibc, clang, glib, and other headers to bindgen search path
-  BINDGEN_EXTRA_CLANG_ARGS =
-    # Includes normal include path
-    (builtins.map (a: ''-I"${a}/include"'') [
-      # add dev libraries here (e.g. pkgs.libvmi.dev)
-      pkgs.glibc.dev
-    ])
-    # Includes with special directory paths
-    ++ [
-      ''-I"${pkgs.llvmPackages_latest.libclang.lib}/lib/clang/${pkgs.llvmPackages_latest.libclang.version}/include"''
-      ''-I"${pkgs.glib.dev}/include/glib-2.0"''
-      ''-I${pkgs.glib.out}/lib/glib-2.0/include/''
-    ];
+  pkgs = import (fetchTarball("https://github.com/NixOS/nixpkgs/archive/85f7e662eda4fa3a995556527c87b2524b691933.tar.gz")) {};
+in pkgs.mkShell {
+  buildInputs = [ pkgs.cargo pkgs.rustc pkgs.diesel-cli pkgs.postgresql.lib ];
 }
