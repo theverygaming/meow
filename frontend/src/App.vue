7c6d5ae8e8bb6df90f4162f54af68a8a460b170c
theverygaming <theverygaming@users.noreply.github.com>
2024-11-10 11:00:58 +0100

    add vuetify

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,5 +1,7 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import { ref } from 'vue';
+
+const drawer = ref(false);
 </script>
 
 <template>
@@ -8,10 +10,23 @@ import HelloWorld from './components/HelloWorld.vue'
   </header>
 
   <main>
-    <img alt="xenia" src="./assets/xenia_drawing1.svg" width="800" />
+    <v-app>
+      <v-app-bar :elevation="2">
+        <template v-slot:prepend>
+          <v-app-bar-nav-icon @click.stop="drawer = !drawer"/>
+        </template>
 
-    <div class="wrapper">
-      <HelloWorld msg=":3" />
-    </div>
+        <v-app-bar-title>Meow</v-app-bar-title>
+      </v-app-bar>
+      <v-navigation-drawer v-model="drawer">
+        <v-list-item link title="Silly" subtitle="test"></v-list-item>
+      </v-navigation-drawer>
+      <v-combobox
+        label="Combobox"
+        :items="['meow', 'dfsdf', ':3']"
+      ></v-combobox>
+      <v-alert text="awawa" type="success"></v-alert>
+    </v-app>
+    <img alt="xenia" src="./assets/xenia_drawing1.svg" width="20%" />
   </main>
 </template>
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,6 +1,26 @@
-//import './assets/main.css'
-
 import { createApp } from 'vue'
+
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+import { md3 } from 'vuetify/blueprints'
+import { aliases, mdi } from 'vuetify/iconsets/mdi-svg'
+
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const vuetify = createVuetify({
+  components,
+  directives,
+  blueprint: md3,
+  icons: {
+    defaultSet: 'mdi',
+    aliases,
+    sets: {
+      mdi,
+    },
+  },
+})
+
+createApp(App).use(vuetify).mount('#app')
