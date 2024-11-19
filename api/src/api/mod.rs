7933afd2c2fcdb1088ef1b7e925267cc73fd5c0f
theverygaming <theverygaming@users.noreply.github.com>
2024-11-19 01:20:18 +0100

    auto-run diesel migrations

--- api/src/api/mod.rs
+++ api/src/api/mod.rs
@@ -9,6 +9,8 @@ fn routes() -> Vec<rocket::Route> {
 
 use crate::db::DbConnection;
 
+const DB_MIGRATIONS: diesel_migrations::EmbeddedMigrations = diesel_migrations::embed_migrations!("./migrations");
+
 #[rocket::launch]
 pub fn rocket() -> _ {
     let figment = rocket::Config::figment().merge((
@@ -16,5 +18,17 @@ pub fn rocket() -> _ {
         std::env::var("DATABASE_URL").expect("DATABASE_URL must be set"),
     ));
 
-    rocket::custom(figment).attach(DbConnection::fairing()).mount("/", routes())
+    rocket::custom(figment)
+        .attach(DbConnection::fairing())
+        .attach(rocket::fairing::AdHoc::on_liftoff("Run migrations", |rocket| {
+            Box::pin(async move {
+                use diesel_migrations::{MigrationHarness};
+                let conn = DbConnection::get_one(rocket).await.unwrap();
+                conn.run(move |c|{
+                    println!("running migrations...");
+                    c.run_pending_migrations(DB_MIGRATIONS).unwrap();
+                }).await;
+            })
+        }))
+        .mount("/", routes())
 }
